c35b0850fab0b0a33bb80b125c5b59aef4c33cb7
Ivo Baptista <ivobass@gmail.com>
2020-04-14 20:27:15 +0100

    Random
    função aleatoria

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,27 +16,26 @@ void main() {
   );
 }
 
-
-
-
 class DicePage extends StatefulWidget {
   @override
   _DicePageState createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 5;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-   
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Click Left...');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) +1;
+                });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -43,9 +43,11 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
                 onPressed: () {
-                  print('Click Right...');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) +1;
+                });
                 },
-                child: Image.asset('images/dice1.png')),
+                child: Image.asset('images/dice$rightDiceNumber.png')),
           ),
         ],
       ),
